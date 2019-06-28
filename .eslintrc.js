d643a8ed95fe3635944a5e27295fbd06c667ef1b
nzws <github@nzws.me>
2019-06-28 19:15:23 +0000

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: ['eslint:recommended', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module'
+  },
+  env: {
+    node: true
+  }
+};
---
+++ index.js
@@ -0,0 +1,8 @@
+// This is a dummy JavaScript file for eslint.
+// Please delete this file if you use this template.
+
+function hello(name) {
+  return `Hello, ${name}!`;
+}
+
+hello('@yuzulabo');
